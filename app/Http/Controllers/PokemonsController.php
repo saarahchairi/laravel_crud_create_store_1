8316e822072f9c07c5eefd2ccc843b359c5746ad
saarahchairi <sarni555@hotmail.com>
2022-08-18 14:55:03 +0200

    fin de l'exo

--- app/Http/Controllers/PokemonsController.php
+++ app/Http/Controllers/PokemonsController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pokemons;
 use Illuminate\Http\Request;
 
 class PokemonsController extends Controller
 {
-    //
+    public function home(){
+        return view("welcome");
+    }
+    public function store(Request $request){
+        $store = new Pokemons;
+        $store->name = $request->name;
+        $store->type = $request->type;
+        $store->niveau = $request->niveau;
+        $store->save();
+        return redirect("/");
+    }
 }
--- database/migrations/2022_08_18_095923_create_pokemons_table.php
+++ database/migrations/2022_08_18_095923_create_pokemons_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('pokemons', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 40);
+            $table->string("type", 50);
+            $table->integer("niveau");
             $table->timestamps();
         });
     }
